98d37ceb7e629d9cbe0b9f84978d2cf0657a54ac
dungdaito <endenislovendenislife1999@gmail.com>
2022-12-18 23:43:05 +0700

    home

--- app/Http/Controllers/client/homepageController.php
+++ app/Http/Controllers/client/homepageController.php
@@ -11,8 +11,16 @@ class homepageController extends Controller
 {
     public function getHomePage()
     {
-        $middle_slides_img = DB::table('sliedes')->where('Is_Top_Slide', 1)->where('Is_Middle_Slide', 0)->get();
+        $middle_slides_img = DB::table('sliedes')->where('Is_Top_Slide', 0)->where('Is_Middle_Slide', 1)->get();
         $top_slides_img = DB::table('sliedes')->where('Is_Top_Slide', 1)->where('Is_Middle_Slide', 0)->get();
-        return view('clientsPage.homePage', ['middle_slides_img' => $middle_slides_img, 'top_slides_img' => $top_slides_img]);
+        $products = DB::table('products')->get()->shuffle();
+        $ran_pro = [];
+        array_push($ran_pro, $products[0], $products[1], $products[2], $products[3]);
+        $dior = DB::table('brand_collections')->where('Brand_ID',2)->get();
+        $chanel = DB::table('brand_collections')->where('Brand_ID',1)->get();
+        $Gucci = DB::table('brand_collections')->where('Brand_ID',3)->get();
+        $LV = DB::table('brand_collections')->where('Brand_ID',4)->get();
+        // dd($chanel);
+        return view('clientsPage.homePage', ['middle_slides_img' => $middle_slides_img, 'top_slides_img' => $top_slides_img, 'randomPro' => $ran_pro,'dior'=>$dior,'channel'=>$chanel,'LV'=>$LV,'gucci'=>$Gucci]);
     }
 }
--- resources/views/clientsPage/homePage.blade.php
+++ resources/views/clientsPage/homePage.blade.php
@@ -19,7 +19,6 @@
                     <button id="prev">
                         <ion-icon name="chevron-back-outline"></ion-icon>
                     </button>
-
                 </div>
                 <div id="nextBut" class="container__slideCol-list-button">
                     <button id="next">
@@ -31,38 +30,16 @@
         <div class="container__featured">
             <div class="container__featured-tittle">Featured Products</div>
             <div class="container__featured-products">
+                @foreach($randomPro as $item)
                 <div class="container__featured-products-items">
-                    <div style="background-image: url(./8858227441694-removebg-preview.png)"
+                    <div style="background-image: url({{ $item->IMG }})"
                         class="container__featured-products-items-img"></div>
                     <div class="container__featured-products-items-info">
-                        <p>Product Name</p>
+                        <p>{{ $item->Name }}</p>
                         <p>$1000</p>
                     </div>
                 </div>
-                <div class="container__featured-products-items">
-                    <div style="background-image: url(./8858227441694-removebg-preview.png)"
-                        class="container__featured-products-items-img"></div>
-                    <div class="container__featured-products-items-info">
-                        <p>Product Name</p>
-                        <p>$1000</p>
-                    </div>
-                </div>
-                <div class="container__featured-products-items">
-                    <div style="background-image: url(./8858227441694-removebg-preview.png)"
-                        class="container__featured-products-items-img"></div>
-                    <div class="container__featured-products-items-info">
-                        <p>Product Name</p>
-                        <p>$1000</p>
-                    </div>
-                </div>
-                <div class="container__featured-products-items">
-                    <div style="background-image: url(./8858227441694-removebg-preview.png)"
-                        class="container__featured-products-items-img"></div>
-                    <div class="container__featured-products-items-info">
-                        <p>Product Name</p>
-                        <p>$1000</p>
-                    </div>
-                </div>
+                @endforeach
             </div>
         </div>
         <div class="container__newArrivals">
@@ -81,54 +58,15 @@
             </div>
             <div class="container__newArrivals-collection">
                 <div id="colListNew" class="container__newArrivals-collection-slide">
+                    @foreach($middle_slides_img as $middle)
                     <div class="container__newArrivals-collection-list"
-                        style="background-image: url(./charles-keith-home-s-banner-1920.jpg);">
+                        style="background-image: url({{ $middle->IMG }});">
                         <div class="content">
                             <div class="tittle">Winter is coming</div>
                             <button>See more >></button>
                         </div>
                     </div>
-                    <div class="container__newArrivals-collection-list" style="background-image: url(./Bag-Chanel.jpg);">
-                        <div class="content">
-                            <div class="tittle">Spring is coming</div>
-                            <button>See more >></button>
-                        </div>
-                    </div>
-                    <div class="container__newArrivals-collection-list"
-                        style="background-image: url(./Chanel-Fashion-Show-Detail-SS-2020.jpg);">
-                        <div class="content">
-                            <div class="tittle">Winter is back</div>
-                            <button>See more >></button>
-                        </div>
-                    </div>
-                    <div class="container__newArrivals-collection-list"
-                        style="background-image: url(./Chanel-Bag-Womenswear.jpg);">
-                        <div class="content">
-                            <div class="tittle">Spring is coming</div>
-                            <button>See more >></button>
-                        </div>
-                    </div>
-                    <div class="container__newArrivals-collection-list"
-                        style="background-image: url(./HeroRegularStandard_GUCCI-GIFT-G.jpg);">
-                        <div class="content">
-                            <div class="tittle">Spring is coming</div>
-                            <button>See more >></button>
-                        </div>
-                    </div>
-                    <div class="container__newArrivals-collection-list"
-                        style="background-image: url(./SS-2020-Bag-Chanel.jpg);">
-                        <div class="content">
-                            <div class="tittle">Winter is back</div>
-                            <button>See more >></button>
-                        </div>
-                    </div>
-                    <div class="container__newArrivals-collection-list"
-                        style="background-image: url(./Bag-Chanel-Spring-Summer-2020.jpg);">
-                        <div class="content">
-                            <div class="tittle">Spring is coming</div>
-                            <button>See more >></button>
-                        </div>
-                    </div>
+                    @endforeach
                     <div class="container__newArrivals-collection-button">
                         <button id="prevNew">
                             <ion-icon name="chevron-back-outline"></ion-icon>
@@ -176,78 +114,90 @@
                     </div>
                 </div>
             </div>
-        </div>
+        </div> 
+        @foreach($channel as $item)
         <div class="container__listCol">
             <div class="container__listCol-items">
                 <div class="container__listCol-items-img">
                     <div class="container__listCol-items-img-child"
-                        style="background-image: url(./chanel/Bag-Chanel-Summer-Trends-2020.jpg);"></div>
+                        style="background-image: url({{ $item->Before_Hover_IMG }});"></div>
                     <div class="container__listCol-items-img-child"
-                        style="background-image: url(./chanel/Chanel-Summer-2020-Bag.jpg);"></div>
+                        style="background-image: url({{ $item->After_Hover_IMG }});"></div>
                     <div class="container__listCol-items-tittle">
                         <button>View More</button>
                     </div>
                 </div>
-
             </div>
+        @endforeach
+        @foreach($dior as $item)
             <div class="container__listCol-items">
                 <div class="container__listCol-items-img">
                     <div class="container__listCol-items-img-child"
-                        style="background-image: url(./dior/Dior-Fall-2020-Runway-Bag-Collection-5.jpg);"></div>
+                        style="background-image: url({{ $item->Before_Hover_IMG }});"></div>
                     <div class="container__listCol-items-img-child"
-                        style="background-image: url(./dior/Dior-Fall-2020-Runway-Bag-Collection.jpg);"></div>
+                        style="background-image: url({{ $item->After_Hover_IMG }});"></div>
                     <div class="container__listCol-items-tittle">
                         <button>View More</button>
                     </div>
                 </div>
-
             </div>
+            @endforeach
+            @foreach($gucci as $item)
             <div class="container__listCol-items">
                 <div class="container__listCol-items-img">
                     <div class="container__listCol-items-img-child"
-                        style="background-image: url(./gucci/63aeece630d8df78acf77978c2161515.jpg);"></div>
-                    <div class="container__listCol-items-img-child" style="background-image: url(./gucci/original.jpg);">
+                        style="background-image: url({{ $item->Before_Hover_IMG }});"></div>
+                    <div class="container__listCol-items-img-child" style="background-image: url({{ $item->After_Hover_IMG }});">
                     </div>
                     <div class="container__listCol-items-tittle">
                         <button>View More</button>
                     </div>
                 </div>
-
             </div>
+            @endforeach
+            @foreach ($LV as $item)
             <div class="container__listCol-items">
                 <div class="container__listCol-items-img">
                     <div class="container__listCol-items-img-child"
-                        style="background-image: url(./LV/W_Fa_Wild_at_Heart_V2.png);"></div>
+                        style="background-image: url({{ $item->Before_Hover_IMG }});"></div>
                     <div class="container__listCol-items-img-child"
-                        style="background-image: url(./LV/W_Fa_Wild_at_Heart_V4.png);"></div>
+                        style="background-image: url({{ $item->After_Hover_IMG }});"></div>
                     <div class="container__listCol-items-tittle">
                         <button>View More</button>
                     </div>
                 </div>
-
             </div>
+            @endforeach
         </div>
         <div class="brands">
+            @foreach($channel as $item)
             <div class="brands__items">
-                <img src="./chanel/kisspng-chanel-no-5-coco-designer-fashion-5b3814a67e60a8.5145910415304019585177.png"
+                <img src="{{ $item->IMG }}"
                     style="height: 100px; width: 100px;" alt="">
                 <div class="brands__items-name">Chanel</div>
             </div>
+            @endforeach
+            @foreach($dior as $item)
             <div class="brands__items">
-                <img src="./dior/kisspng-christian-dior-se-parfums-christian-dior-miss-dior-cognitive-5b2e17476580a9.5535051615297472714158.png"
+                <img src="{{ $item->IMG }}"
                     style="height: 100px; width: 100px;" alt="">
                 <div class="brands__items-name">Dior</div>
             </div>
+            @endforeach
+            @foreach ($gucci as $item)
             <div class="brands__items">
-                <img src="./gucci/kisspng-gucci-milan-fashion-week-italian-fashion-logo-logo-gucci-5b23156f47e071.8334917815290259032944.png"
+                <img src="{{ $item->IMG }}"
                     style="height: 60px; width: 100px;" alt="">
                 <div class="brands__items-name">Gucci</div>
             </div>
+            @endforeach
+            @foreach($LV as $item)
             <div class="brands__items">
-                <img src="./LV/kisspng-lvmh-brand-logo-clothing-fashion-logo-gucci-5b492e7b831ea3.3283494915315226835371.png"
+                <img src="{{ $item->IMG }}"
                     style="height: 100px; width: 100px;" alt="">
                 <div class="brands__items-name">Louis Vuitton</div>
             </div>
+            @endforeach
         </div>
         <div class="subscribeUs">
             <div class="subscribeUs__text">
