ddba848579cdaced1332a917d450dafe38009908
dungdaito <endenislovendenislife1999@gmail.com>
2022-12-20 22:55:18 +0700

    hehe

--- app/Http/Controllers/client/clientProductController.php
+++ app/Http/Controllers/client/clientProductController.php
@@ -121,11 +121,12 @@ class clientProductController extends Controller
     public function getSpecificProduct(Request $req)
     {
         $Slug = $req->Slug;
-        $this_product = DB::table('Products')->join('Product_details', 'Products.ID', '=', 'product_details.Product_ID')->where('product_details.Slug', $Slug)->get();
+        $this_product = DB::table('Products')->join('Product_details', 'Products.ID', '=', 'product_details.Product_ID')->where('product_details.Slug',$Slug)->get();
         $random_products = DB::table('products')->join('product_details', 'products.ID', '=', 'product_details.Product_ID')->get()->shuffle();
         $ran_pro = $random_products->take(4);
         $product_ID =  $this_product[0]->Product_ID;
         $get_color = DB::table('Products')->join('Product_details', 'Products.ID', '=', 'product_details.Product_ID')->where('product_details.Product_ID', $product_ID)->get();
+        
         return view('clientsPage.mainProduct', ['product' => $this_product, 'getColor' => $get_color, 'ran_pro' => $ran_pro]);
     }
 }
