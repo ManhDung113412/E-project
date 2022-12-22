e78883711b0b906e666f16fe1d527131be0e908f
dungdaito <endenislovendenislife1999@gmail.com>
2022-12-22 21:56:07 +0700

    hehe

--- app/Http/Controllers/client/clientProductController.php
+++ app/Http/Controllers/client/clientProductController.php
@@ -5,11 +5,13 @@ namespace App\Http\Controllers\client;
 use App\Http\Controllers\Controller;
 use App\Models\Cart;
 use App\Models\Product;
+use App\Models\ProductDetail;
 use Illuminate\Http\Request;
 use DB;
 use App\Models\Category;
 use Illuminate\Support\Facades\Auth;
 use App\Models\User;
+use App\Models\Brand;
 
 class clientProductController extends Controller
 {
@@ -19,122 +21,258 @@ class clientProductController extends Controller
     }
 
 
-    public function getLongWallet()
+    public function getLongWallet(Request $req)
     {
+        $keyWord = $req->searchBox;
+
         $products = DB::table('products')->join('product_details', 'products.ID', '=', 'product_details.Product_ID')->get()->shuffle();
         $cart_quantity = session()->get('cart_quantity');
-
         $ran_pro = $products->take(4);
-        $longWallet = DB::table('categories')->join('Products', 'Products.Category_ID', '=', 'categories.ID')->join('product_details', 'Products.ID', '=', 'product_details.Product_ID')->where('categories.ID', 3)->groupBy('Product_details.Product_ID')->paginate(13);
+        if (strlen($keyWord) > 0) {
+            $longWallet = Category::join('Products', 'products.Category_ID', '=', 'categories.ID')
+                ->join('product_details', 'products.ID', '=', 'product_details.Product_ID')
+                ->where('Products.Name', 'like', '%' . $keyWord . '%')
+                ->where('categories.ID', 3)
+                ->groupBy('Product_details.Product_ID')
+                ->get();
+        } else {
+            $longWallet = DB::table('categories')
+                ->join('Products', 'Products.Category_ID', '=', 'categories.ID')
+                ->join('product_details', 'Products.ID', '=', 'product_details.Product_ID')->where('categories.ID', 3)->groupBy('Product_details.Product_ID')
+                ->paginate(13);
+        }
+
         return view('layouts.longWallet', ['longWallet' => $longWallet, 'randomProduct' => $ran_pro, 'cart_quantity' => $cart_quantity]);
     }
 
 
-    public function getSmallWallet()
+    public function getSmallWallet(Request $req)
     {
+        $keyWord = $req->searchBox;
 
         $products = DB::table('products')->join('product_details', 'products.ID', '=', 'product_details.Product_ID')->get()->shuffle();
         $cart_quantity = session()->get('cart_quantity');
-
         $ran_pro = $products->take(4);
-        $smallWallet = DB::table('categories')->join('Products', 'Products.Category_ID', '=', 'categories.ID')->join('product_details', 'Products.ID', '=', 'product_details.Product_ID')->where('categories.ID', 4)->groupBy('Product_details.Product_ID')->paginate(13);
+
+        if (strlen($keyWord) > 0) {
+            $smallWallet = Category::join('Products', 'products.Category_ID', '=', 'categories.ID')
+                ->join('product_details', 'products.ID', '=', 'product_details.Product_ID')
+                ->where('Products.Name', 'like', '%' . $keyWord . '%')
+                ->where('categories.ID', 4)
+                ->groupBy('Product_details.Product_ID')
+                ->get();
+        } else {
+            $smallWallet = DB::table('categories')
+                ->join('Products', 'Products.Category_ID', '=', 'categories.ID')
+                ->join('product_details', 'Products.ID', '=', 'product_details.Product_ID')
+                ->where('categories.ID', 4)
+                ->groupBy('Product_details.Product_ID')
+                ->paginate(13);
+        }
+
         return view('layouts.smallWallet', ['smallWallet' => $smallWallet, 'randomProduct' => $ran_pro, 'cart_quantity' => $cart_quantity]);
     }
 
-    public function getCardsHolder()
+    public function getCardsHolder(Request $req)
     {
+        $keyWord = $req->searchBox;
 
         $products = DB::table('products')->join('product_details', 'products.ID', '=', 'product_details.Product_ID')->get()->shuffle();
         $cart_quantity = session()->get('cart_quantity');
-
         $ran_pro = $products->take(4);
-        $cardHolder =  DB::table('categories')->join('Products', 'Products.Category_ID', '=', 'categories.ID')->join('product_details', 'Products.ID', '=', 'product_details.Product_ID')->where('categories.ID', 1)->groupBy('Product_details.Product_ID')->paginate(13);
+
+        if (strlen($keyWord) > 0) {
+            $cardHolder = Category::join('Products', 'products.Category_ID', '=', 'categories.ID')
+                ->join('product_details', 'products.ID', '=', 'product_details.Product_ID')
+                ->where('Products.Name', 'like', '%' . $keyWord . '%')
+                ->where('categories.ID', 1)
+                ->groupBy('Product_details.Product_ID')
+                ->get();
+        } else {
+            $cardHolder = DB::table('categories')
+                ->join('Products', 'Products.Category_ID', '=', 'categories.ID')
+                ->join('product_details', 'Products.ID', '=', 'product_details.Product_ID')->where('categories.ID', 1)
+                ->groupBy('Product_details.Product_ID')
+                ->paginate(13);
+        }
+
         return view('layouts.cardsHolder', ['cardsHolder' => $cardHolder, 'randomProduct' => $ran_pro, 'cart_quantity' => $cart_quantity]);
     }
 
 
-    public function getchainandStrap()
+    public function getchainandStrap(Request $req)
     {
+        $keyWord = $req->searchBox;
+
         $products = DB::table('products')->join('product_details', 'products.ID', '=', 'product_details.Product_ID')->get()->shuffle();
         $cart_quantity = session()->get('cart_quantity');
-
         $ran_pro = $products->take(4);
-        $chainAndStrap = DB::table('categories')->join('Products', 'Products.Category_ID', '=', 'categories.ID')->join('product_details', 'Products.ID', '=', 'product_details.Product_ID')->where('categories.ID', 2)->groupBy('Product_details.Product_ID')->paginate(13);
+
+        if (strlen($keyWord) > 0) {
+            $chainAndStrap = Category::join('Products', 'products.Category_ID', '=', 'categories.ID')
+                ->join('product_details', 'products.ID', '=', 'product_details.Product_ID')
+                ->where('Products.Name', 'like', '%' . $keyWord . '%')
+                ->where('categories.ID', 2)
+                ->groupBy('Product_details.Product_ID')
+                ->get();
+        } else {
+            $chainAndStrap = DB::table('categories')
+                ->join('Products', 'Products.Category_ID', '=', 'categories.ID')
+                ->join('product_details', 'Products.ID', '=', 'product_details.Product_ID')->where('categories.ID', 2)
+                ->groupBy('Product_details.Product_ID')
+                ->paginate(13);
+        }
         return view('layouts.chainsandStrap', ['chainAndStrap' => $chainAndStrap, 'randomProduct' => $ran_pro, 'cart_quantity' => $cart_quantity]);
     }
 
-    public function getGucci()
+    public function getGucci(Request $req)
     {
+        $keyWord = $req->searchBox;
 
         $products = DB::table('products')->join('product_details', 'products.ID', '=', 'product_details.Product_ID')->get()->shuffle();
         $cart_quantity = session()->get('cart_quantity');
-
         $ran_pro = $products->take(4);
-        $gucci = DB::table('brands')->join('products', 'Products.Brand_ID', '=', 'Brands.ID')->join('product_details', 'Products.ID', '=', 'product_details.Product_ID')->where('Products.Brand_ID', 4)->groupBy('Product_details.Product_ID')->paginate(13);
+        if (strlen($keyWord) > 0) {
+            $gucci = Brand::join('Products', 'brands.ID', '=', 'Products.Brand_ID')
+                ->join('product_details', 'products.ID', '=', 'product_details.Product_ID')
+                ->where('Products.Name', 'like', '%' . $keyWord . '%')
+                ->where('Products.Brand_ID', 4)
+                ->groupBy('Product_details.Product_ID')
+                ->get();
+        } else {
+            $gucci = DB::table('brands')
+                ->join('products', 'Products.Brand_ID', '=', 'Brands.ID')
+                ->join('product_details', 'Products.ID', '=', 'product_details.Product_ID')->where('Products.Brand_ID', 4)
+                ->groupBy('Product_details.Product_ID')
+                ->paginate(13);
+        }
         return view('layouts.gucci', ['gucci' => $gucci, 'randomProduct' => $ran_pro, 'cart_quantity' => $cart_quantity]);
     }
 
 
-    public function getLouisVuiton()
+    public function getLouisVuiton(Request $req)
     {
+        $keyWord = $req->searchBox;
 
         $products = DB::table('products')->join('product_details', 'products.ID', '=', 'product_details.Product_ID')->get()->shuffle();
         $cart_quantity = session()->get('cart_quantity');
-
         $ran_pro = $products->take(4);
-        $louisVuiton = DB::table('brands')->join('products', 'Products.Brand_ID', '=', 'Brands.ID')->join('product_details', 'Products.ID', '=', 'product_details.Product_ID')->where('Products.Brand_ID', 3)->groupBy('Product_details.Product_ID')->paginate(13);
+
+        if (strlen($keyWord) > 0) {
+            $louisVuiton = Brand::join('Products', 'brands.ID', '=', 'Products.Brand_ID')
+                ->join('product_details', 'products.ID', '=', 'product_details.Product_ID')
+                ->where('Products.Name', 'like', '%' . $keyWord . '%')
+                ->where('Products.Brand_ID', 3)
+                ->groupBy('Product_details.Product_ID')
+                ->get();
+        } else {
+            $louisVuiton = DB::table('brands')
+                ->join('products', 'Products.Brand_ID', '=', 'Brands.ID')
+                ->join('product_details', 'Products.ID', '=', 'product_details.Product_ID')->where('Products.Brand_ID', 3)
+                ->groupBy('Product_details.Product_ID')
+                ->paginate(13);
+        }
         return view('layouts.louisVuiton', ['louisVuiton' => $louisVuiton, 'randomProduct' => $ran_pro, 'cart_quantity' => $cart_quantity]);
     }
 
 
-    public function getChannel()
+    public function getChannel(Request $req)
     {
+        $keyWord = $req->searchBox;
 
         $products = DB::table('products')->join('product_details', 'products.ID', '=', 'product_details.Product_ID')->get()->shuffle();
         $cart_quantity = session()->get('cart_quantity');
-
         $ran_pro = $products->take(4);
-        $channel = DB::table('brands')->join('products', 'Products.Brand_ID', '=', 'Brands.ID')->join('product_details', 'Products.ID', '=', 'product_details.Product_ID')->where('Products.Brand_ID', 1)->groupBy('Product_details.Product_ID')->paginate(13);
+        if (strlen($keyWord) > 0) {
+            $channel = Brand::join('Products', 'brands.ID', '=', 'Products.Brand_ID')
+                ->join('product_details', 'products.ID', '=', 'product_details.Product_ID')
+                ->where('Products.Name', 'like', '%' . $keyWord . '%')
+                ->where('Products.Brand_ID', 1)
+                ->groupBy('Product_details.Product_ID')
+                ->get();
+        } else {
+            $channel = DB::table('brands')
+                ->join('products', 'Products.Brand_ID', '=', 'Brands.ID')
+                ->join('product_details', 'Products.ID', '=', 'product_details.Product_ID')
+                ->where('Products.Brand_ID', 1)
+                ->groupBy('Product_details.Product_ID')
+                ->paginate(13);
+        }
+
         return view('layouts.Channel', ['Channel' => $channel, 'randomProduct' => $ran_pro, 'cart_quantity' => $cart_quantity]);
     }
-    public function getDior()
+    public function getDior(Request $req)
     {
+        $keyWord = $req->searchBox;
 
         $products = DB::table('products')->join('product_details', 'products.ID', '=', 'product_details.Product_ID')->get()->shuffle();
-
         $ran_pro = $products->take(4);
         $cart_quantity = session()->get('cart_quantity');
+        if (strlen($keyWord) > 0) {
+            $dior = Brand::join('Products', 'brands.ID', '=', 'Products.Brand_ID')
+                ->join('product_details', 'products.ID', '=', 'product_details.Product_ID')
+                ->where('Products.Name', 'like', '%' . $keyWord . '%')
+                ->where('Products.Brand_ID', 2)
+                ->groupBy('Product_details.Product_ID')
+                ->get();
+        } else {
+            $dior = DB::table('brands')
+                ->join('products', 'Products.Brand_ID', '=', 'Brands.ID')
+                ->join('product_details', 'Products.ID', '=', 'product_details.Product_ID')
+                ->where('Products.Brand_ID', 2)
+                ->groupBy('Product_details.Product_ID')
+                ->paginate(13);
+        }
 
-        $dior = DB::table('brands')->join('products', 'Products.Brand_ID', '=', 'Brands.ID')->join('product_details', 'Products.ID', '=', 'product_details.Product_ID')->where('Products.Brand_ID', 2)->groupBy('Product_details.Product_ID')->paginate(13);
         return view('layouts.dior', ['dior' => $dior, 'randomProduct' => $ran_pro, 'cart_quantity' => $cart_quantity]);
     }
 
     public function getNewArrival(Request $req)
     {
-        
-
+        $keyWord = $req->searchBox;
 
         $products = DB::table('products')->join('product_details', 'products.ID', '=', 'product_details.Product_ID')->get()->shuffle();
         $ran_pro = $products->take(4);
         $cart_quantity = session()->get('cart_quantity');
 
-        $products = DB::table('products')->join('product_details', 'products.ID', '=', 'product_details.Product_ID')->where('product_details.Is_New_Arrivals', 1)->paginate(13);
+        if (strlen($keyWord) > 0) {
+            $products = Product::where('Products.Name', 'like', '%' . $keyWord . '%')
+                ->join('product_details', 'products.ID', '=', 'product_details.Product_ID')
+                ->where('product_details.Is_New_Arrivals', 1)->get();
+        } else {
+            $products = DB::table('products')->join('product_details', 'products.ID', '=', 'product_details.Product_ID')->where('product_details.Is_New_Arrivals', 1)->paginate(13);
+        }
+
         return view('layouts.newArrival', ['products' => $products, 'randomProduct' => $ran_pro, 'cart_quantity' => $cart_quantity]);
     }
-    public function getTrending()
+    public function getTrending(Request $req)
     {
-        $products = DB::table('products')->join('product_details', 'products.ID', '=', 'product_details.Product_ID')->get()->shuffle();
+        $keyWord = $req->searchBox;
 
+        $products = DB::table('products')
+            ->join('product_details', 'products.ID', '=', 'product_details.Product_ID')
+            ->get()
+            ->shuffle();
         $ran_pro = $products->take(4);
         $cart_quantity = session()->get('cart_quantity');
 
-        $products = DB::table('products')->join('product_details', 'products.ID', '=', 'product_details.Product_ID')->where('product_details.Is_Trending', 1)->groupBy('Product_details.Product_ID')->paginate(13);
+        if (strlen($keyWord) > 0) {
+            $products = Product::where('Products.Name', 'like', '%' . $keyWord . '%')
+                ->join('product_details', 'products.ID', '=', 'product_details.Product_ID')
+                ->where('product_details.Is_Trending', 1)
+                ->groupBy('Product_details.Product_ID')->get();
+        } else {
+            $products = DB::table('products')->join('product_details', 'products.ID', '=', 'product_details.Product_ID')->where('product_details.Is_Trending', 1)->groupBy('Product_details.Product_ID')->paginate(13);
+        }
+
         return view('layouts.trending', ['products' => $products, 'randomProduct' => $ran_pro, 'cart_quantity' => $cart_quantity]);
     }
 
     public function getDiscount()
     {
-        $products = DB::table('products')->join('product_details', 'products.ID', '=', 'product_details.Product_ID')->get()->shuffle();
+        $products = DB::table('products')
+        ->join('product_details', 'products.ID', '=', 'product_details.Product_ID')
+        ->get()->shuffle();
 
         $ran_pro = $products->take(4);
         $cart_quantity = session()->get('cart_quantity');
@@ -147,13 +285,19 @@ class clientProductController extends Controller
     public function getSpecificProduct(Request $req)
     {
         $Slug = $req->Slug;
-        $this_product = DB::table('Products')->join('Product_details', 'Products.ID', '=', 'product_details.Product_ID')->where('product_details.Slug', $Slug)->get();
-        $random_products = DB::table('products')->join('product_details', 'products.ID', '=', 'product_details.Product_ID')->get()->shuffle();
+        $this_product = DB::table('Products')
+        ->join('Product_details', 'Products.ID', '=', 'product_details.Product_ID')
+        ->where('product_details.Slug', $Slug)->get();
+        $random_products = DB::table('products')
+        ->join('product_details', 'products.ID', '=', 'product_details.Product_ID')
+        ->get()->shuffle();
         $ran_pro = $random_products->take(4);
 
         $product_ID =  $this_product[0]->Product_ID;
 
-        $get_color = DB::table('Products')->join('Product_details', 'Products.ID', '=', 'product_details.Product_ID')->where('product_details.Product_ID', $product_ID)->get();
+        $get_color = DB::table('Products')
+        ->join('Product_details', 'Products.ID', '=', 'product_details.Product_ID')
+        ->where('product_details.Product_ID', $product_ID)->get();
         $cart_quantity = session()->get('cart_quantity');
         return view('clientsPage.mainProduct', ['product' => $this_product, 'getColor' => $get_color, 'ran_pro' => $ran_pro, 'cart_quantity' => $cart_quantity]);
     }
