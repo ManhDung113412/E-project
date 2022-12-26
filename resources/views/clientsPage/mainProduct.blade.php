2c68b2abdd86cf8886294a56c10d9ac18b6bc0da
unknown <haihd.work@gmail.com>
2022-12-26 15:58:44 +0700

    a

--- resources/views/clientsPage/mainProduct.blade.php
+++ resources/views/clientsPage/mainProduct.blade.php
@@ -58,7 +58,7 @@
                                 <li><b>Code:</b> {{ $item->Code }}</p>
                                 <li><b>In Stock:</b> {{ $item->Quantity }}</li>
                             </ul>
-                            <a href="{{ url('/client/products/specificProduct/pdf', $item->Slug)  }}">Download Pdf</a>
+                            <a href="{{ url('/client/products/specificProduct/pdf', $item->Slug)  }}"><ion-icon name="download-outline"></ion-icon></a>
                         </div>
                     </div>
                 </div>
--- resources/views/layouts/pdf.blade.php
+++ resources/views/layouts/pdf.blade.php
@@ -6,9 +6,79 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Details</title>
-    <link rel="stylesheet" href="{{ asset('styles/pageStyle/pdf.css') }}">
+    {{-- <link rel="stylesheet" href="{{ asset('styles/pageStyle/pdf.css') }}"> --}}
 </head>
+<style>
 
+
+
+.container{
+    width: 90%;
+    height: 100%;
+    margin: 0 auto;
+    /* overflow: hidden; */
+}
+
+.container__header{
+    width: 100%;
+    margin-top: 50px;
+}
+
+.container__header div:nth-child(1){
+    font-size: 35px;
+}
+.container__header div:nth-child(2){
+    font-size: 18px;
+    margin-left: 40px;
+}
+
+.container__content{
+    width: 100%;
+    margin-top: 60px;
+}
+
+.container__content-title{
+    width: 100%;
+    font-size: 30px;
+    font-weight: bold;
+    text-align: center;
+}
+
+.container__content-details{
+    width: 100%;
+    margin-top: 40px;
+}
+
+.container__content-details-img{
+    width: 30%;
+    height: 300px;
+    float: left;
+    background-color: rgb(218, 213, 213);
+}
+
+.container__content-details-info{
+    width: 70%;
+    float: left;
+}
+
+table{
+    margin-left: 20px;
+    text-align: left;
+    font-size: 18px;
+}
+
+table tr th{
+    width: 150px;
+    height: 25px;
+
+}
+
+table tr th td{
+    width: 150px;
+    /* overflow-y: auto; */
+}
+
+</style>
 <body>
     <form action="">
         <div class="container">
