e932a3b6d4fe33e6463184f05c0af6d5c55887cd
dungdaito <endenislovendenislife1999@gmail.com>
2022-12-27 12:33:14 +0700

    heehe

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -35,21 +35,27 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Paginator::useBootstrap();
-        View::composer('*',function($view){
+        View::composer('*', function ($view) {
             $customer_ID = Auth::guard('users')->id();
-            $this_customer = User::where('id', $customer_ID)->get();
+            $this_customer = DB::table('users')
+                ->where('id', $customer_ID)
+                ->get();
+
             $carts = DB::table('carts As c')
-            ->join('product_details as pd', 'c.Product_Detail_ID', 'pd.ID')
-            ->join('products as p', 'pd.Product_ID', 'p.ID')
-            ->select('Export_Price', 'Sale_Price', 'Main_IMG', 'Name', 'Color', 'Product_Detail_ID', 'Product_quantity', DB::raw('sum(c.Product_quantity * pd.Export_Price) as subtotal'))
-            ->where('Customer_ID', $customer_ID)
-            ->groupBy('Export_Price', 'Sale_Price', 'Main_IMG', 'Name', 'Color', 'Product_Detail_ID', 'Product_quantity')
-            ->get();
+                ->join('product_details as pd', 'c.Product_Detail_ID', 'pd.ID')
+                ->join('products as p', 'pd.Product_ID', 'p.ID')
+                ->select('Export_Price', 'Sale_Price', 'Main_IMG', 'Name', 'Color', 'Product_Detail_ID', 'Product_quantity', DB::raw('sum(c.Product_quantity * pd.Export_Price) as subtotal'))
+                ->where('Customer_ID', $customer_ID)
+                ->groupBy('Export_Price', 'Sale_Price', 'Main_IMG', 'Name', 'Color', 'Product_Detail_ID', 'Product_quantity')
+                ->get();
 
             $cart_quantity = count($carts);
-            $wishList_quantity = count(WishList::where('Customer_ID',$customer_ID)->get());
-            // dd($this_customer);
-            $view->with(['customer_cart'=>$carts,'cart_quantity'=>$cart_quantity,'wishList_quantity'=>$wishList_quantity,'customer_infor'=>$this_customer[0]]);
-        });  
+            $wishList_quantity = count(WishList::where('Customer_ID', $customer_ID)->get());
+            // foreach($this_customer as $item){
+            //     $user_name = $item->username;
+            // }
+            // dd($user_name);
+            $view->with(['customer_cart' => $carts, 'cart_quantity' => $cart_quantity, 'wishList_quantity' => $wishList_quantity,'this_customer'=>$this_customer]);
+        });
     }
 }
--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -65,8 +65,8 @@
                     </div>
                 </div>
             </div>
-            <div id="openLog" class="header__log">
-                @if (Auth::guard('users')->check())
+            @if (Auth::guard('users')->check())
+                <div id="openLog" class="header__log">
                     <div class="header__log-signed" style="">
                         <div class="header__log-signed-profile">
                             <a href="{{ url('client/myProfile') }}">My Profile</a>
@@ -75,7 +75,90 @@
                             <a href="{{ url('client/logout') }}">Sign Out</a>
                         </div>
                     </div>
-                @else
+                </div>
+                <div id="shoppingCart" class="header__cart">
+                    <div class="header__cart-tittle">Shopping Cart
+                        <button id="hideCart">
+                            <ion-icon name="chevron-up-outline"></ion-icon>
+                        </button>
+                    </div>
+                    <div class="header__cart-list">
+                        @foreach ($customer_cart as $item)
+                            <div class="header__cart-list-items">
+                                <div class="header__cart-list-items-img"
+                                    style="background-image: url('{{ $item->Main_IMG }}')"></div>
+                                <div class="header__cart-list-items-info">
+                                    <div class="header__cart-list-items-info-name">{{ $item->Name }}</div>
+                                    <div class="header__cart-list-items-info-quantity">
+                                        <button>
+                                            <ion-icon name="remove-outline"></ion-icon>
+                                        </button>
+                                        <div class="header__cart-list-items-info-quantity-num">
+                                            {{ $item->Product_quantity }}</div>
+                                        <a>
+                                            <ion-icon name="add-outline"></ion-icon>
+                                        </a>
+                                    </div>
+                                </div>
+                                <div class="header__cart-list-items-info-right">
+                                    <div class="header__cart-list-items-info-right-price">${{ $item->Export_Price }}
+                                    </div>
+                                    <div class="header__cart-list-items-info-right-action">
+                                        <a href="{{ url('/client/Cart/removefromcart', $item->Product_Detail_ID) }}">
+                                            <ion-icon name="trash-bin-outline"></ion-icon>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        @endforeach
+                    </div>
+                    <div class="header__cart-total">
+                        <p><b>Total</b></p>
+                        <p>$10000</p>
+                    </div>
+                    <div class="header__cart-checkout">
+                        <button>Check out</button>
+                    </div>
+                </div>
+                <div class="header__update">
+                    <div id="topUpdate" class="header__update-all">
+                        <div class="header__update-1"><a style="color: white" href=""> Sale up to 50%..</a></div>
+                        <div class="header__update-1"><a style="color: white" href=""> Give code for..</a></div>
+                        <div class="header__update-1"><a style="color: white" href=""> Free ship if..</a></div>
+                    </div>
+                </div>
+                <div class="header__nav">
+                    <div class="header__nav-menu">
+                        <button style="background-color: transparent; border: none" id="openMenu">
+                            <ion-icon style="font-size: 40px;" name="menu-outline"></ion-icon>
+                            <p>Menu</p>
+                        </button>
+                    </div>
+                    <div class="header__nav-logo">
+                        <a style="color: black" href="{{ url('client/home') }}">
+                            P U R S E L L E T
+                        </a>
+                    </div>
+                    <div class="header__nav-right">
+
+                        <button id="log">
+                            <div>{{ $this_customer[0]->username }}</div>
+                            <ion-icon name="person"></ion-icon>
+                        </button>
+                        <button id="showCart">
+                            <div class="quantityCart">{{ $cart_quantity }}</div>
+                            <ion-icon name="cart-outline"></ion-icon>
+                            </a>
+                            <button>
+                                <div class="quantityCart">{{ $wishList_quantity }}</div>
+                                <ion-icon name="heart-outline"></ion-icon>
+                            </button>
+                    </div>
+                </div>
+
+            @else
+
+                <div id="openLog" class="header__log">
                     <div class="header__log-notSign">
                         <div class="header__log-notSign-signIn">
                             <a href="{{ url('client/login') }}">Sign In</a>
@@ -84,54 +167,88 @@
                             <a href="">Sign Up</a>
                         </div>
                     </div>
-                @endif
-            </div>
-            <div id="shoppingCart" class="header__cart">
-                <div class="header__cart-tittle">Shopping Cart
-                    <button id="hideCart">
-                        <ion-icon name="chevron-up-outline"></ion-icon>
-                    </button>
                 </div>
-                <div class="header__cart-list">
-                    @foreach ($customer_cart as $item)
-                    <div class="header__cart-list-items">
-                            <div class="header__cart-list-items-img"
-                                style="background-image: url('{{ $item->Main_IMG }}')"></div>
+                <div id="shoppingCart" class="header__cart">
+                    <div class="header__cart-tittle">Shopping Cart
+                        <button id="hideCart">
+                            <ion-icon name="chevron-up-outline"></ion-icon>
+                        </button>
+                    </div>
+                    <div class="header__cart-list">
+                        <div class="header__cart-list-items">
+                            <div class="header__cart-list-items-img" style="background-image: url('')"></div>
                             <div class="header__cart-list-items-info">
-                                <div class="header__cart-list-items-info-name">{{ $item->Name }}</div>
+                                <div class="header__cart-list-items-info-name"></div>
                                 <div class="header__cart-list-items-info-quantity">
                                     <button>
                                         <ion-icon name="remove-outline"></ion-icon>
                                     </button>
                                     <div class="header__cart-list-items-info-quantity-num">
-                                        {{ $item->Product_quantity }}</div>
-                                    <a >
+                                    </div>
+                                    <a>
                                         <ion-icon name="add-outline"></ion-icon>
                                     </a>
                                 </div>
                             </div>
-                        <div class="header__cart-list-items-info-right">
-                            <div class="header__cart-list-items-info-right-price">${{ $item->Export_Price }}</div>
-                            <div class="header__cart-list-items-info-right-action">
-                                <a href="{{ url('/client/Cart/removefromcart', $item->Product_Detail_ID) }}">
-                                    <ion-icon name="trash-bin-outline"></ion-icon>
-                                </a>
+                            <div class="header__cart-list-items-info-right">
+                                <div class="header__cart-list-items-info-right-price">
+                                </div>
+                                <div class="header__cart-list-items-info-right-action">
+                                    <a href="">
+                                        <ion-icon name="trash-bin-outline"></ion-icon>
+                                    </a>
+                                </div>
                             </div>
                         </div>
-
                     </div>
-                    @endforeach
+                    <div class="header__cart-total">
+                        <p><b>Total</b></p>
+                        <p>$10000</p>
+                    </div>
+                    <div class="header__cart-checkout">
+                        <button>Check out</button>
+                    </div>
+                </div>
+                <div class="header__update">
+                    <div id="topUpdate" class="header__update-all">
+                        <div class="header__update-1"><a style="color: white" href=""> Sale up to 50%..</a>
+                        </div>
+                        <div class="header__update-1"><a style="color: white" href=""> Give code for..</a>
+                        </div>
+                        <div class="header__update-1"><a style="color: white" href=""> Free ship if..</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="header__nav">
+                    <div class="header__nav-menu">
+                        <button style="background-color: transparent; border: none" id="openMenu">
+                            <ion-icon style="font-size: 40px;" name="menu-outline"></ion-icon>
+                            <p>Menu</p>
+                        </button>
+                    </div>
+                    <div class="header__nav-logo">
+                        <a style="color: black" href="{{ url('client/home') }}">
+                            P U R S E L L E T
+                        </a>
+                    </div>
+                    <div class="header__nav-right">
 
+                        <button id="log">
+                            <ion-icon name="person-outline"></ion-icon>
+                        </button>
+                        <button id="showCart">
+                            <div class="quantityCart">{{ $cart_quantity }}</div>
+                            <ion-icon name="cart-outline"></ion-icon>
+                            </a>
+                            <button>
+                                <div class="quantityCart">{{ $wishList_quantity }}</div>
+                                <ion-icon name="heart-outline"></ion-icon>
+                            </button>
+                    </div>
                 </div>
-                <div class="header__cart-total">
-                    <p><b>Total</b></p>
-                    <p>$10000</p>
-                </div>
-                <div class="header__cart-checkout">
-                    <button>Check out</button>
-                </div>
-            </div>
-            <div class="header__update">
+            @endif
+
+            {{-- <div class="header__update">
                 <div id="topUpdate" class="header__update-all">
                     <div class="header__update-1"><a style="color: white" href=""> Sale up to 50%..</a></div>
                     <div class="header__update-1"><a style="color: white" href=""> Give code for..</a></div>
@@ -151,25 +268,22 @@
                     </a>
                 </div>
                 <div class="header__nav-right">
-                    <div>{{ $customer_infor->Last_Name }} {{ $customer_infor->First_Name }}</div>
+
                     <button id="log">
-                        @if (Auth::guard('users')->check())
-                        <ion-icon name="person"></ion-icon>
-                        @else
-                        <ion-icon name="person-outline"></ion-icon>
-                        @endif
+                            <ion-icon name="person"></ion-icon>
+                            <ion-icon name="person-outline"></ion-icon>
                     </button>
                     <button id="showCart">
-                        <div class="quantityCart">{{ $cart_quantity }}</div>
+                            <div class="quantityCart">{{ $cart_quantity }}</div>
                         <ion-icon name="cart-outline"></ion-icon>
                         </a>
                         <button>
-                            <div class="quantityCart">{{ $wishList_quantity }}</div>
+                                <div class="quantityCart">{{ $wishList_quantity }}</div>
                             <ion-icon name="heart-outline"></ion-icon>
                         </button>
                 </div>
             </div>
-        </div>
+        </div> --}}
     </form>
 
 </body>
