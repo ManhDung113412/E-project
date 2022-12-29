0b4cd49f82dd77f6149629c85e3b5c648f7053a4
dungdaito <endenislovendenislife1999@gmail.com>
2022-12-29 21:15:00 +0700

    hehe

---
+++ app/Http/Controllers/EmailController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class EmailController extends Controller
+{
+    public function recoverPassword(Request $req){
+         
+
+        $details = [
+            'title' => 'Recover Your Password From Pursellet'
+
+        ];
+    }
+}
