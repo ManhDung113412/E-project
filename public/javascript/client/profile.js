89fab5e4462e2ec32c71fa9e57da97ec86e7f9a5
unknown <haihd.work@gmail.com>
2022-12-30 23:57:13 +0700

    a

--- public/javascript/client/profile.js
+++ public/javascript/client/profile.js
@@ -4,8 +4,25 @@ document.getElementById('edit').onclick = function (e) {
     document.getElementById('showOff').classList.add('off');
 }
 
+document.getElementById('change').onclick = function (e) {
+    e.preventDefault();
+    document.getElementById('showChange').classList.add('active');
+    document.getElementById('showOff').classList.add('off');
+}
+
 document.getElementById('offEdit').onclick = function (e) {
     e.preventDefault();
     document.getElementById('showEdit').classList.remove('active');
     document.getElementById('showOff').classList.remove('off');
+}
+
+document.getElementById('offChange').onclick = function (e) {
+    e.preventDefault();
+    document.getElementById('showChange').classList.remove('active');
+    document.getElementById('showOff').classList.remove('off');
+}
+
+document.getElementById('showMenu').onclick = function (e) {
+    e.preventDefault();
+    document.getElementById('menu').classList.toggle('abc');
 }
