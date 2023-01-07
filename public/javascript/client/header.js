78727b0dec6ab5edbf787c2dfc7d587f7c2e2d3a
unknown <haihd.work@gmail.com>
2023-01-07 07:14:54 +0700

    a

--- public/javascript/client/header.js
+++ public/javascript/client/header.js
@@ -71,3 +71,51 @@ document.getElementById('hideCart').onclick = function (e) {
     document.getElementById('shoppingCart').classList.toggle('activeCart');
     document.getElementById('openLog').classList.remove('active');
 }
+
+
+
+// drop small menu
+
+// object.onmousemove = function(){myScript};
+
+var hoverMenu = document.getElementById('dropSmallMenu');
+var dropMenu = document.getElementById('showSmallMenu');
+
+hoverMenu.onmouseover = function () {
+    dropMenu.style.display = 'block';
+}
+hoverMenu.onmouseout = function () {
+    dropMenu.style.display = 'none';
+}
+dropMenu.onmouseover = function () {
+    dropMenu.style.display = 'block';
+}
+dropMenu.onmouseout = function () {
+    dropMenu.style.display = 'none';
+}
+
+// scroll to show
+
+var prevScroll = window.pageYOffset;
+window.addEventListener("scroll", function () {
+    var currentScroll = window.pageYOffset;
+    if (prevScroll < currentScroll) {
+        // document.getElementById("smallHeader").style.top = "0";
+        // document.getElementById("smallHeader").style.display = "block";
+        document.getElementById('smallHeader').classList.add('showSmallMenu');
+        document.getElementById('header__side').classList.remove('activeMenu');
+        document.getElementById('drop1').classList.remove('header__side-category-active');
+        document.getElementById('drop2').classList.remove('header__side-collection-active');
+        document.getElementById('drop3').classList.remove('header__side-brand-active');
+        document.getElementById('dropdown2').classList.remove('icon2');
+        document.getElementById('dropdown1').classList.remove('icon1');
+        document.getElementById('dropdown3').classList.remove('icon3');
+    } else {
+        document.getElementById('smallHeader').classList.remove('showSmallMenu');
+        dropMenu.style.display = 'none';
+
+        // document.getElementById("smallHeader").style.top = "-55px";
+    }
+    prevScroll = currentScroll;
+
+})
